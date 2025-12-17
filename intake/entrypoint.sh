cf49c097cd122033ec81232e9f3ce7118e67e8f6
Vincent Li <vinceli@unc.edu>
2025-12-17 14:41:10 -0500

    Change dockerfile for deployment

---
+++ intake/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+if [ -n "$GITHUB_PRIVATE_KEY_PEM" ]; then
+  echo "$GITHUB_PRIVATE_KEY_PEM" > /tmp/github-app-private-key.pem
+  chmod 600 /tmp/github-app-private-key.pem
+  export GITHUB_PRIVATE_KEY_PATH=/tmp/github-app-private-key.pem
+fi
+
+exec "$@"
